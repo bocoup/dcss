a332d909d4ec80a78c79cdffe02b1f8b08787388
Erika Miguel-Yeo <erika@bocoup.com>
2019-08-21 14:26:15 -0400

    adding express session

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const path = require('path');
 
-const app = express()
+const app = express();
+const session = require('express-session')
 const port = process.env.SERVER_PORT || 5000;
+
+app.use(session({
+    secret: 'mit tsl teacher moments',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
-
