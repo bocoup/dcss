4dafa2bd1629c89174fe66dec02145ac58ba61bf
Erika Miguel-Yeo <erika@bocoup.com>
2019-08-21 15:58:52 -0400

    simple login flow stub

--- src/index.js
+++ src/index.js
@@ -10,6 +10,21 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.post('/login', (req, res) => {
+    req.session.username = 'boo';
+    res.send('ok');
+});
+
+app.get('/me', (req, res) => {
+    if (!req.session.username) res.send('Not logged in!');
+    res.send(req.session.username);
+});
+
+app.post('/logout', (req, res) => {
+    delete req.session.username;
+    res.send('ok');
+});
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
