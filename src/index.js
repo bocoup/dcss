8d3826325278444f273dc52aea37b408bcc36442
Erika Miguel-Yeo <erika@bocoup.com>
2019-08-22 11:09:39 -0400

    adding simple login logout ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
 const app = express();
-const session = require('express-session')
+const cors = require('cors');
+const session = require('express-session');
 const port = process.env.SERVER_PORT || 5000;
 const { Pool } = require('pg');
 
 const pgSession = require('connect-pg-simple')(session);
-const pool = new Pool();
+const pool = new Pool({
+    host: 'localhost',
+    user: 'tm',
+    password: 'teachermoments',
+    database: 'teachermoments',
+    port: 5432
+});
+
+app.use(cors());
 
 app.use(session({
     secret: 'mit tsl teacher moments',
@@ -15,14 +24,14 @@ app.use(session({
     store: new pgSession({pool})
 }));
 
-app.post('/login', (req, res) => {
+app.post('/login', cors() , (req, res) => {
     req.session.username = 'boo';
-    res.send('ok');
+    res.send({ ok: true, username: req.session.username});
 });
 
 app.get('/me', (req, res) => {
     if (!req.session.username) res.send('Not logged in!');
-    res.send(req.session.username);
+    res.send({username: req.session.username});
 });
 
 app.post('/logout', (req, res) => {
