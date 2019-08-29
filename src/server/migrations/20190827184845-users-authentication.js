83b658a58fcfb335fb325098e6f803bff23f7341
Erika Miguel-Yeo <erika@bocoup.com>
2019-08-29 15:48:44 -0400

    tab on squery

--- src/server/migrations/20190827184845-users-authentication.js
+++ src/server/migrations/20190827184845-users-authentication.js
@@ -2,25 +2,25 @@
 
 exports.up = function(db) {
     return db.runSql(`
-    ALTER TABLE users
-    ADD COLUMN id SERIAL PRIMARY KEY,
-    ADD COLUMN email TEXT,
-    ADD COLUMN username TEXT,
-    ADD COLUMN hash TEXT,
-    ADD COLUMN salt TEXT;
+        ALTER TABLE users
+        ADD COLUMN id SERIAL PRIMARY KEY,
+        ADD COLUMN email TEXT,
+        ADD COLUMN username TEXT,
+        ADD COLUMN hash TEXT,
+        ADD COLUMN salt TEXT;
     `);
 };
 
 exports.down = function(db) {
     return db.runSql(`
-    DELETE FROM users
-    RETURNING *;
-    ALTER TABLE users
-    DROP COLUMN id,
-    DROP COLUMN email,
-    DROP COLUMN username,
-    DROP COLUMN hash,
-    DROP COLUMN salt;
+        DELETE FROM users
+        RETURNING *;
+        ALTER TABLE users
+        DROP COLUMN id,
+        DROP COLUMN email,
+        DROP COLUMN username,
+        DROP COLUMN hash,
+        DROP COLUMN salt;
     `);
 };
 
