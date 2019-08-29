710d43262e6a516cb28142782aa5ad5838ad1778
Erika Miguel-Yeo <erika@bocoup.com>
2019-08-29 15:47:02 -0400

    formatting sql

--- src/server/migrations/20190827184845-users-authentication.js
+++ src/server/migrations/20190827184845-users-authentication.js
@@ -1,20 +1,19 @@
 'use strict';
 
 exports.up = function(db) {
-    return db.runSql(
-        `ALTER TABLE users
+    return db.runSql(`
+    ALTER TABLE users
     ADD COLUMN id SERIAL PRIMARY KEY,
     ADD COLUMN email TEXT,
     ADD COLUMN username TEXT,
     ADD COLUMN hash TEXT,
     ADD COLUMN salt TEXT;
-    `
-    );
+    `);
 };
 
 exports.down = function(db) {
-    return db.runSql(
-        `DELETE FROM users
+    return db.runSql(`
+    DELETE FROM users
     RETURNING *;
     ALTER TABLE users
     DROP COLUMN id,
@@ -22,8 +21,7 @@ exports.down = function(db) {
     DROP COLUMN username,
     DROP COLUMN hash,
     DROP COLUMN salt;
-    `
-    );
+    `);
 };
 
 exports._meta = {
