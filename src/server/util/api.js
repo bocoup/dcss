40a7528c11d0ca709cae10539bbc2a90671e4482
Erika Miguel-Yeo <erika@bocoup.com>
2019-09-03 15:14:34 -0400

    adding comments

--- src/server/util/api.js
+++ src/server/util/api.js
@@ -4,5 +4,6 @@ exports.apiError = (res, error) => {
     return res.status(status).json({ error: true, message });
 };
 
+// This function wraps around async middlewares and catches errors
 exports.asyncMiddleware = middle => (req, res, next) =>
     Promise.resolve(middle(req, res, next)).catch(e => exports.apiError(e));
