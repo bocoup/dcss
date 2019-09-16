98b31c604ab4f8f9dfdc63478f453703a7c08403
Erika Miguel-Yeo <erika@bocoup.com>
2019-09-16 16:25:55 -0400

    slide

--- src/client/components/slide/index.jsx
+++ src/client/components/slide/index.jsx
@@ -4,3 +4,7 @@ import './slide.css'
 export const Slide = (props) => (
     <div className="slide" />
 );
+
+export const MiniSlide = (props) => (
+    <Slide className="mini_slide" />
+);
--- src/client/stories/slide.stories.jsx
+++ src/client/stories/slide.stories.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Slide } from '../components/slide';
+import { Slide, MiniSlide } from '../components/slide';
 
 storiesOf('Slide', module)
     .add('plain slide', () => 
         <Slide />
+    )
+    .add('mini slide', () => 
+        <MiniSlide />
     )
