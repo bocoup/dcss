5077f4a2f6ba97592b11be9d893748cb9acc85df
Katie Broida <kbroida@gmail.com>
2019-09-30 16:46:35 -0400

    Connect scenario API to UI, add error handling in UI

--- client/components/editor/index.jsx
+++ client/components/editor/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Tab } from 'semantic-ui-react';
-import { ScenarioEditor} from '@components/scenarioEditor';
+import ScenarioEditor from '@components/scenarioEditor';
 import { SlideEditor } from '@components/slideEditor';
 
 import './editor.css';
 
 const panes = [
-    { menuItem: 'Moment', render: ScenarioEditor},
-    { menuItem: 'Slides', render: SlideEditor}
+    { menuItem: 'Moment', render: () => <ScenarioEditor /> },
+    { menuItem: 'Slides', render: SlideEditor }
 ];
 
-const Editor = () => (
-    <Tab panes={panes} />
-);
+const Editor = () => <Tab panes={panes} />;
 
 export default hot(module)(Editor);
