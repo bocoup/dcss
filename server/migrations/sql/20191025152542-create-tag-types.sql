4d926840672966e7da25c8f49529b1acd0d7693e
Erika Miguel-Yeo <erika@bocoup.com>
2019-10-25 13:50:43 -0400

    adding data to tags and tag_types

--- server/migrations/sql/20191025152542-create-tag-types.sql
+++ server/migrations/sql/20191025152542-create-tag-types.sql
@@ -2,6 +2,11 @@ CREATE TABLE tag_types(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK(name <> '')
 );
+
+INSERT INTO tag_types (name)
+VALUES
+ ('categories'),
+ ('topics');
 -- Up above
 ---
 -- Down below
--- server/migrations/sql/20191025170040-create-tags-table.sql
+++ server/migrations/sql/20191025170040-create-tags-table.sql
@@ -1,9 +1,14 @@
 CREATE TABLE tags(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK(name <> ''),
-    type_type_id INT NOT NULL,
-    FOREIGN KEY(type_type_id) REFERENCES tag_types(id)
+    tag_type_id INT NOT NULL,
+    FOREIGN KEY(tag_type_id) REFERENCES tag_types(id)
 );
+
+INSERT INTO tags (name, tag_type_id)
+VALUES 
+ ('official', 1), 
+ ('community', 1);
 -- Up above
 ---
 -- Down below
