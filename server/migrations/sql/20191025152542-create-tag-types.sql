703714b837c6f8408d358f3c290b275fb5674d43
Erika Miguel-Yeo <erika@bocoup.com>
2019-10-25 14:04:06 -0400

    making singular table names and single migration

--- server/migrations/sql/20191025152542-create-tag-types.sql
+++ server/migrations/sql/20191025152542-create-tag-types.sql
@@ -1,13 +1,32 @@
-CREATE TABLE tag_types(
+CREATE TABLE tag_type (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK(name <> '')
 );
 
-INSERT INTO tag_types (name)
+INSERT INTO tag_type (name)
 VALUES
  ('categories'),
  ('topics');
+
+ CREATE TABLE tag (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL CHECK(name <> ''),
+    tag_type_id INT NOT NULL REFERENCES tag_type(id)
+);
+
+INSERT INTO tag (name, tag_type_id)
+VALUES
+ ('official', 1),
+ ('community', 1);
+
+CREATE TABLE scenario_tag (
+    scenario_id INT NOT NULL REFERENCES scenario(id),
+    tag_id INT NOT NULL REFERENCES tag(id),
+    PRIMARY KEY (scenario_id, tag_id)
+);
 -- Up above
 ---
 -- Down below
-DROP TABLE "tag_types";
+DROP TABLE "tag_type";
+DROP TABLE "tag";
+DROP TABLE "scenario_tag";
