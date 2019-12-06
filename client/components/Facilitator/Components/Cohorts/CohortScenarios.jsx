fe5970642407899d8080143b5bca5a3405749bbb
Rick Waldron <waldron.rick@gmail.com>
2019-12-06 00:29:06 -0500

    Drag/Drop Sorting: various improvements to address user experience issues.

--- client/components/Facilitator/Components/Cohorts/CohortScenarios.jsx
+++ client/components/Facilitator/Components/Cohorts/CohortScenarios.jsx
@@ -164,6 +164,11 @@ export class CohortScenarios extends React.Component {
                             className="cohort__scrolling-tbody"
                             onChange={onChangeOrder}
                             ref={this.tableBody}
+                            options={{
+                                direction: 'vertical',
+                                swapThreshold: 0.5,
+                                animation: 150
+                            }}
                         >
                             {orderCorrectedScenarios.map((scenario, index) => {
                                 const checked = cohort.scenarios.includes(
