3d33cc8e8daeb9b3d325c0ae37170446480b837a
Rick Waldron <waldron.rick@gmail.com>
2019-12-19 16:22:11 -0500

    Account Administration: make it impossible to remove "Participant" permissions from any user

--- client/components/AccountAdmin/index.jsx
+++ client/components/AccountAdmin/index.jsx
@@ -35,7 +35,7 @@ class AccountAdmin extends Component {
 
         return (
             <React.Fragment>
-                <h1>Account Admin</h1>
+                <h1>Account Administration</h1>
                 <Table celled>
                     <Table.Header>
                         <Table.Row>
